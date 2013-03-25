5a4e8f83a8c2c43924ddf1e35b75069896616f25
Jarmo <jamo@isotalo.fi>
2013-03-25 12:18:08 +0200

    Update tmc-check-example.c

--- setti1/SampleCheckProject/test/tmc-check-example.c
+++ setti1/SampleCheckProject/test/tmc-check-example.c
@@ -27,8 +27,8 @@ int main(int argc, const char *argv[])
     tmc_register_test(s, test_foo, "1.1 1.2");
     tmc_register_test(s, test_bar, "1.2");
     tmc_register_test(s, test_lib_function, "1.3");
-    /*
+
     tmc_register_memtest(test_foo, 1, 1);
-    */
+
     return tmc_run_tests(argc, argv, s);
 }
